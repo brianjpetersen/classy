146556a8dcdf067dcf7aea2a97fe4dc6cfff7c23
Brian J Petersen <brianjpetersen@gmail.com>
2015-06-02 01:41:57 -0400

    finished debugging context manager

--- classy/application.py
+++ classy/application.py
@@ -17,19 +17,20 @@ logger.setLevel(logging.INFO)
 
 class Application:
 
-    configuration = {}
+    configuration = {'logger': logger}
     routes = {}
     
     def configure(self):
-        def _configure(Controller, configuration):
-            configuration = copy.deepcopy(configuration)
-            Controller.configure(configuration)
+        def _configure(Controller):
+            Controller.__configure__(self.configuration)
             for _, potential_controller in inspect.getmembers(Controller):
-                if isinstance(potential_controller, controllers.Controller):
+                if not isinstance(potential_controller, type):
+                    continue
+                if issubclass(potential_controller, controllers.Controller):
                     child_controller = potential_controller
-                    _configure(child_controller, Controller.configuration)
+                    _configure(child_controller)
         for Controller in self.routes.values():
-            _configure(Controller, self.configuration)
+            _configure(Controller)
         
     def __call__(self, environ, start_response):
         request = webob.Request(environ)
@@ -37,39 +38,30 @@ class Application:
         # extract relevant details from request
         url = request.path
         method = request.method.lower()
+        # find appropriate handler
+        Controller, args, kwargs = routing.match(self.routes, url, method)
+        if Controller is None:
+            response = exceptions.HTTPNotFound()
+            return response(environ, start_response)
+        # instantiate controller and set up handler context
         try:
-            # find appropriate handler
-            Controller, args, kwargs = routing.match(self.routes, url, method)
-            if Controller is None:
-                raise exceptions.HTTPNotFound
+            controller = Controller(request, response, self.configuration)
+        except:
+            response = classy.exceptions.HTTPServiceUnavailable()
+            return response(environ, start_response)
+        with controller:
             # Controller configuration should allow this method
+            # NB: this could go outside the with context, but including it here
+            # captures all exceptions except 404 and those related to instantiation
             allowed_methods = getattr(Controller, 'allowed_methods', set())
             if method not in allowed_methods:
                 raise exceptions.HTTPMethodNotAllowed
-            # instantiate controller and extract appropriate handler
-            controller = Controller(request, response)
+            # extract appropriate handler (this should never fail)
             handler = getattr(controller, method)
-            # execute handler and view
-            try:
-                returned = handler(*args, **kwargs)
-                controller.view(returned)
-            except:
-                raise
-            else:
-                log = self.format_log(request, response)
-                logger.info(log)
-        except exceptions.HTTPException as http_exception:
-            response = http_exception
-            log = self.format_log(request, response)
-            logger.warning(log)
-        except Exception as exception:
-            http_exception = exceptions.HTTPInternalServerError
-            response = http_exception
-            log = self.format_err(traceback.format_exc(), request, response)
-            logger.error(log)
-        finally:
-            controller.response = response
-            return response(environ, start_response)
+            # call controller and view method
+            returned = handler(*args, **kwargs)
+            controller.__view__(returned)
+        return controller.response(environ, start_response)
 
     def add_route(self, route, Controller):
         route = utilities.Url(route)
@@ -81,16 +73,6 @@ class Application:
     def serve(self, *args, **kwargs):
         self.configure()
         waitress.serve(self, *args, **kwargs)
-
-    def format_log(self, request, response):
-        now = datetime.datetime.utcnow().isoformat()[:23]
-        return '{} {} {} {} {}'.format(now, request.client_addr.ljust(15),
-                                       response.status_code,
-                                       request.method,
-                                       request.path)
-
-    def format_err(self, err, request, response):
-        return '\n{}\n\n{}'.format(self.format_log(request, response), err)
 
 
 app = application = Application()
--- classy/controllers.py
+++ classy/controllers.py
@@ -1,9 +1,11 @@
 # standard libraries
-pass
+import datetime
+import logging
+import traceback
 # third party libraries
 pass
 # first party libraries
-from . import utilities
+from . import (utilities, exceptions)
 
 
 __all__ = ('Controller', )
@@ -21,10 +23,10 @@ class Controller:
         self.configuration = configuration
 
     @staticmethod
-    def configure(configuration):
+    def __configure__(configuration):
         pass
 
-    def view(self, returned):
+    def __view__(self, returned):
         if isinstance(returned, str):
             self.response.text = returned
         elif isinstance(returned, bytes):
@@ -33,8 +35,43 @@ class Controller:
     def __enter__(self):
         return self
         
-    def __exit__(self, *args):
-        pass
+    def __exit__(self, exception_type, exception_value, exception_traceback):
+        logger = self.configuration.get('logger', None)
+        if logger is None:
+            logger = logging.NullHandler()
+        # log and handle any errors
+        if exception_value is None:
+            log = self.__log__(exception_type, exception_value, 
+                               exception_traceback)
+            logger.info(log)
+        elif isinstance(exception_value, exceptions.HTTPException):
+            self.response = exception_value
+            log = self.__log__(exception_type, exception_value, 
+                               exception_traceback)
+            logger.warning(log)
+        elif isinstance(exception_value, Exception):
+            self.response = exceptions.HTTPInternalServerError()
+            log = self.__log__(exception_type, exception_value, 
+                               exception_traceback)
+            logger.error(log)
+        # suppress exception reraise in calling routine
+        return True
+        
+    def __log__(self, *exception_tuple):
+        exception_is_none = any(map(lambda e: e is None, exception_tuple))
+        if exception_is_none:
+            exception_message = ''
+        else:
+            exception_message = traceback.format_exception(*exception_tuple)
+            exception_message = '\n\n    ' + '    '.join(exception_message)
+        now = datetime.datetime.utcnow().isoformat()[:23]
+        status = self.response.status_code
+        client_address = self.request.client_addr
+        method = self.request.method
+        path = self.request.path
+        return '{} {} {} {} {} {}'.format(now, client_address.ljust(15),
+                                          status, method, path, 
+                                          exception_message)
     
     get = utilities._raise_method_not_allowed
     head = utilities._raise_method_not_allowed
